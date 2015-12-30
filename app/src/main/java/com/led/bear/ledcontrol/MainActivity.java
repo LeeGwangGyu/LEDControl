a85fcdd9c48c3494743689220052d24b5343d811
Pengyu Zhang <965006619@qq.com>
2015-12-30 17:38:27 +0800

    Add Bluetooth Scan

--- app/src/main/java/com/led/bear/ledcontrol/MainActivity.java
+++ app/src/main/java/com/led/bear/ledcontrol/MainActivity.java
@@ -1,12 +1,18 @@
 package com.led.bear.ledcontrol;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
 
 
 public class MainActivity extends FragmentActivity {
+
+    private Button button;
+    private static final int REQUEST_CONNECT_DEVICE_SECURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +23,7 @@ public class MainActivity extends FragmentActivity {
         // 设置左滑菜单
         SlidingMenu menu = new SlidingMenu(this);
         menu.setMode(SlidingMenu.LEFT);
-        //设置滑动的屏幕范围，该设置为全屏区域都可以滑动
+        // 设置滑动的屏幕范围，该设置为全屏区域都可以滑动
         menu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
         // 设置阴影图片
         // menu.setShadowDrawable(R.drawable.menu_color);
@@ -36,5 +42,27 @@ public class MainActivity extends FragmentActivity {
         menu.attachToActivity(this, SlidingMenu.SLIDING_CONTENT);
         //为侧滑菜单设置布局
         menu.setMenu(R.layout.layout_configmenu);
+
+
+
+        button =(Button)findViewById(R.id.button2);
+        button.setOnClickListener(button_click);
     }
+
+    View.OnClickListener button_click = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.button2:
+                    // Intent serverIntent = new Intent(MainActivity.this,DeviceListActivity.class);
+                    // startActivityForResult(serverIntent, REQUEST_CONNECT_DEVICE_SECURE);
+                    Intent bluetooth = new Intent();
+                    bluetooth.setClass(MainActivity.this, DeviceListActivity.class);
+                    startActivity(bluetooth);
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
 }
